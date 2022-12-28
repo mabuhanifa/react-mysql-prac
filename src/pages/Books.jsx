5f24b5a04a903708f84951e25f8bdce5fb5d8f85
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-29 04:45:26 +0600

    books

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -6,11 +6,22 @@ export default function Books() {
     const fetchBooks = async () => {
       const res = await fetch("http://localhost:5000/books");
       const data = await res.json();
-      console.log(data)
+      console.log(data);
       setBooks(data);
     };
     fetchBooks();
   }, []);
- 
-  return <div>Books</div>;
+
+  return (
+    <div>
+      {books &&
+        books.map((book, i) => (
+          <div key={i}>
+            <h1>{book.title}</h1>
+            <p>{book.desc}</p>
+            <p>{book.cover}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
